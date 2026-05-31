4382156f04b50419f8805230dd537d47430feed6
AliteIz <lyz_ydz_xz@outlook.com>
2026-05-31 21:59:33 +0800

    更新

--- install.sh
+++ install.sh
@@ -1,4 +1,5 @@
 #!/data/data/com.termux/files/usr/bin/bash -e
+cd $"{HOME}"
 
 echo "[1/3] 🧼 更换 Termux 镜像源..."
 termux-change-repo
@@ -9,9 +10,10 @@ pkg install -y x11-repo
 pkg install -y libtalloc proot xz-utils aria2 pv ffplay wget curl
 
 echo "[3/3] 🌐 下载 Kali NetHunter一键部署脚本..."
-curl -LO https://github.com/AliteIz/Kali_NetHunter_Termux/raw/master/Install_Lyz.bin
+curl -LO https://raw.githubusercontent.com/AliteIz/Kali_NetHunter_Termux/refs/heads/main/Install_Lyz.bin
+
 echo "赋予脚本执行权限"
 chmod +x ./Install_Lyz.bin
+
 echo "运行脚本"
 bash ./Install_Lyz.bin
-
